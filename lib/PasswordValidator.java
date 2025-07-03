a32334a513dad4a2ce8e5be6660c8db2331d7299
Poohh-494 <sasipong.cha@ku.th>
2025-07-03 19:48:24 +0700

    Add files via upload

--- lib/PasswordValidator.java
+++ lib/PasswordValidator.java
@@ -5,15 +5,35 @@ public class PasswordValidator {
     /**
      * คุณจะต้องเขียน Javadoc ที่สมบูรณ์ที่นี่ในอนาคต
      * เพื่ออธิบายกฎการทำงานของเมธอด
+     * @param password string ที่ต้องการตรวจสอบ
+     * @return PassswordStringth INVALID อื่นๆ
      */
-    // TODO: แก้ไข return type ของเมธอดนี้ให้ถูกต้อง
-    public static void validate(String password) { // Function Type ให้เป็น PasswordStrength 
+    public static PasswordStrength validate(String password) { // Function Type ให้เป็น PasswordStrength 
+        int minLength = 8;
+        boolean isLowerCase = false,isUperCase = false;
+        if(password==null || password.length()<minLength)
+            return PasswordStrength.INVALID;
+
         
-        // ส่วนของ Implementation ที่คุณต้องเขียน
-        // ...
-        
-        int minLength = 8 // TODO: มีอะไรขาดหายไปที่บรรทัดนี้?
-        
-        return null ; // TODO: การคืนค่านี้ถูกต้องหรือไม่?
+        for(char c : password.toCharArray()){
+            if(Character.isLowerCase(c)){
+                isLowerCase = true;
+            }
+        }
+        for(char c : password.toCharArray()){
+            if(Character.isUpperCase(c)){
+                isUperCase = true;
+            }
+        }
+        if(isLowerCase == true && isUperCase == true){
+            return PasswordStrength.MEDIUM;
+        }
+         if(isLowerCase == true || isUperCase == true){
+            return PasswordStrength.WEAK;
+        } 
+        if(isLowerCase == false && isUperCase == false){
+            return PasswordStrength.STRONG;
+        }
+        return PasswordStrength.STRONG;
     }
 }
--- lib/PasswordValidatorTest.java
+++ lib/PasswordValidatorTest.java
@@ -6,14 +6,42 @@ public class PasswordValidatorTest{
         System.out.println("--- Running Password Validator Tests ---");
         
         // Test Case 1: รหัสผ่านสั้นควรจะ INVALID
-        PasswordStrength result1 = PasswordValidator.validate("123");
+        String pw = "123";
+        PasswordStrength result1 = PasswordValidator.validate(pw);
         if (result1 == PasswordStrength.INVALID) {
-            System.out.println("Test Case 1 Passed: Short password is INVALID.");
+            System.out.println("Test Case 1 : "+pw+" : Passed: Short password is INVALID.");
         } else {
-            System.out.println("Test Case 1 FAILED: Expected INVALID but got " + result1);
+            System.out.println("Test Case 1 : "+pw+" : FAILED: Expected INVALID but got " + result1);
         }
-
         // --- เขียน Test Case อื่นๆ ต่อ ---
+        pw = "aaaaaaaa";
+        PasswordStrength result2 = PasswordValidator.validate(pw);
+        if (result2 == PasswordStrength.WEAK) {
+            System.out.println("Test Case 2 : "+pw+" : Passed: All LowerCase password is WEAK.");
+        } else {
+            System.out.println("Test Case 1 : "+pw+" : FAILED: Expected WEAK but got " + result2);
+        }
+        pw = "AAAAAAAA";
+        PasswordStrength result3 = PasswordValidator.validate(pw);
+        if (result3 == PasswordStrength.WEAK) {
+            System.out.println("Test Case 2 : "+pw+" : Passed: All UpperCase password is WEAK.");
+        } else {
+            System.out.println("Test Case 1 : "+pw+" : FAILED: Expected WEAK but got " + result3);
+        }
+        pw = "PooHfdDOOL";
+        PasswordStrength result4 = PasswordValidator.validate(pw);
+        if (result4 == PasswordStrength.MEDIUM) {
+            System.out.println("Test Case 2 : "+pw+" : Passed: All LowerCase and Uppercase password is MEDIUM.");
+        } else {
+            System.out.println("Test Case 1 : "+pw+" : FAILED: Expected MEDIUM but got " + result4);
+        }
+        pw = "!@#$%^&*";
+        PasswordStrength result5 = PasswordValidator.validate(pw);
+        if (result5 == PasswordStrength.STRONG) {
+            System.out.println("Test Case 2 : "+pw+" : Passed: Password is STRONG.");
+        } else {
+            System.out.println("Test Case 1 : "+pw+" : FAILED: Expected STRONG but got " + result5);
+        }
 
         System.out.println("--------------------------------");
     }
